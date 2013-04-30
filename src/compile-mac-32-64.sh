06286061c8bcda11df14225d81439e9bb4a03d22
Jiri Srba <srba@cs.aau.dk>
2013-04-30 20:26:58 +0200

    updated the compile script for mac so that it is compatible up to 10.6 (snow leopard)

--- src/compile-mac-32-64.sh
+++ src/compile-mac-32-64.sh
@@ -10,17 +10,19 @@ lib64="-L$HOME/dev/ia64/uppaal/lib -ludbm"
 inc32="-I$HOME/dev/ia32/include -I$HOME/dev/ia32/uppaal/include"
 lib32="-L$HOME/dev/ia32/uppaal/lib -ludbm"
 
+compatibility="-mmacosx-version-min=10.6"
+
 src=`find . -name "*.cpp"`
 
 #flex -o $parser/Generated/lexer.cpp $parser/flex.ll
 bison -o $parser/Generated/parser.cpp $parser/grammar.yy
 
 g++ -DBOOST_DISABLE_THREADS -DNDEBUG -DDISABLE_ASSERTX -O3 -Wall -mtune=core2 \
-    $src $inc64 $lib64 -o verifytapn64 && \
+    $src $inc64 $lib64 $compatibility -o verifytapn64 && \
 echo "64-bit mac OK" &
 
 g++ -DBOOST_DISABLE_THREADS -DNDEBUG -DDISABLE_ASSERTX -O3 -Wall -mtune=core2 -m32 \
-    $src $inc32 $lib32 -o verifytapn32 && \
+    $src $inc32 $lib32 $compatibility -o verifytapn32 && \
 echo "32-bit mac OK"  &
 
 wait
