14485455f2b54086e359d160f20fbe4f9c5554a3
Kenneth Yrke Jørgensen <kenneth@yrke.dk>
2022-02-03 16:01:59 +0100

    Fixed trace export

--- src/ReachabilityChecker/Trace/TraceStore.cpp
+++ src/ReachabilityChecker/Trace/TraceStore.cpp
@@ -3,6 +3,36 @@
 #include "../../Core/SymbolicMarking/SymbolicMarking.hpp"
 
 #include <rapidxml.hpp>
+/* Adding declarations to make it compatible with gcc 4.7 and greater */
+namespace rapidxml { namespace internal {
+    template<class OutIt, class Ch>
+    inline OutIt print_children(OutIt out, const xml_node <Ch> *node, int flags, int indent);
+
+    template<class OutIt, class Ch>
+    inline OutIt print_attributes(OutIt out, const xml_node <Ch> *node, int flags);
+
+    template<class OutIt, class Ch>
+    inline OutIt print_data_node(OutIt out, const xml_node <Ch> *node, int flags, int indent);
+
+    template<class OutIt, class Ch>
+    inline OutIt print_cdata_node(OutIt out, const xml_node <Ch> *node, int flags, int indent);
+
+    template<class OutIt, class Ch>
+    inline OutIt print_element_node(OutIt out, const xml_node <Ch> *node, int flags, int indent);
+
+    template<class OutIt, class Ch>
+    inline OutIt print_declaration_node(OutIt out, const xml_node <Ch> *node, int flags, int indent);
+
+    template<class OutIt, class Ch>
+    inline OutIt print_comment_node(OutIt out, const xml_node <Ch> *node, int flags, int indent);
+
+    template<class OutIt, class Ch>
+    inline OutIt print_doctype_node(OutIt out, const xml_node <Ch> *node, int flags, int indent);
+
+    template<class OutIt, class Ch>
+    inline OutIt print_pi_node(OutIt out, const xml_node <Ch> *node, int flags, int indent);
+} }
+#include <rapidxml_print.hpp>
 
 namespace VerifyTAPN
 {
@@ -178,6 +208,6 @@ namespace VerifyTAPN
 			UpdateMarking(marking, traceInfo, tapn);
 		}
 
-		std::cerr << doc.first_node();
+		std::cout << doc;
 	}
 }
